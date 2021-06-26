9548d77c49d1917cd4f38a7160e50739c3be4a79
adam-p <77075408+adam-pietka@users.noreply.github.com>
2021-06-26 16:05:36 +0200

    Adding an initial implementation of class CardController and CardDto

---
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -0,0 +1,49 @@
+package com.kodilla.ecommercee.controller;
+
+
+import com.kodilla.ecommercee.domain.CartDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/vi/card")
+public class CartController {
+
+    @GetMapping(value = "getCard")
+    public CartDto getCard(@RequestParam long cardId) throws CardNotFoundExeption {
+
+//    fixed data  - return object for test purpose only
+        CartDto cartDto2 = new CartDto(1, new BigDecimal(55.3), false);
+        CartDto cartDto3 = new CartDto(1, new BigDecimal(55.3), false);
+
+        if (cardId == 1L){ return new CartDto(1, new BigDecimal(55.3), false); }
+        if (cardId == 2L){ return new CartDto(2, new BigDecimal(185.35), false); }
+        if (cardId == 3L){ return new CartDto(3, new BigDecimal(333.00), false); }
+        if (cardId == 4L){ return new CartDto(4, new BigDecimal(2_499.99), false); }
+
+        throw new CardNotFoundExeption();
+    }
+
+    @DeleteMapping(value = "deleteCard")
+    public boolean deleteCard(@RequestParam long cardId ){
+
+        if (cardId <= 4L){ return true; }
+
+        return false;
+    }
+
+    @PutMapping(value = "updateCard")
+    public CartDto updateCard(@RequestBody CartDto cartDto) throws CardNotFoundExeption {
+
+        if (cartDto.getCardId() <= 4L){ return cartDto; }
+
+        throw new CardNotFoundExeption();
+    }
+
+    @PostMapping(value = "createCard")
+    public CartDto createCard(@RequestBody CartDto cartDto){
+        return cartDto;
+    }
+}
---
+++ src/main/java/com/kodilla/ecommercee/domain/CartDto.java
@@ -0,0 +1,48 @@
+package com.kodilla.ecommercee.domain;
+
+import java.math.BigDecimal;
+
+public class CartDto {
+
+    private long cardId;
+    private BigDecimal cartSum;
+    private boolean isCartClosed;
+
+    public CartDto(int id, BigDecimal cartSum, boolean isCartClosed) {
+        this.cardId = id;
+        this.cartSum = cartSum;
+        this.isCartClosed = isCartClosed;
+    }
+
+    public long getCardId() {
+        return cardId;
+    }
+
+    public BigDecimal getCartSum() {
+        return cartSum;
+    }
+
+    public boolean isCartClosed() {
+        return isCartClosed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CartDto)) return false;
+
+        CartDto cartDto = (CartDto) o;
+
+        if (cardId != cartDto.cardId) return false;
+        if (isCartClosed != cartDto.isCartClosed) return false;
+        return cartSum != null ? cartSum.equals(cartDto.cartSum) : cartDto.cartSum == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (cardId ^ (cardId >>> 32));
+        result = 31 * result + (cartSum != null ? cartSum.hashCode() : 0);
+        result = 31 * result + (isCartClosed ? 1 : 0);
+        return result;
+    }
+}
