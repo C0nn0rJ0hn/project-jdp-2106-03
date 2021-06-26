e380f77ed69ca733729375e385526f9973e3aea0
Paweł Piętoń <pawel.pieton@gmail.com>
2021-06-26 21:59:55 +0200

    JDP210603-1 Product Controller - initial implementation of endpoints

---
+++ src/main/java/com/kodilla/ecommercee/domain/dto/ProductDto.java
@@ -0,0 +1,67 @@
+package com.kodilla.ecommercee.domain.dto;
+
+import com.kodilla.ecommercee.domain.ProductCondition;
+
+public class ProductDto
+{
+    private Long id;
+    private String name;
+    private String shortDesc;
+    private String longDesc;
+    private double productPrice;
+    private int quantityOnStore;
+    private ProductCondition productCondition;
+    private double productWeight;
+    private boolean stillOnSale;
+
+    public ProductDto(Long id, String name, String shortDesc, String longDesc, double productPrice,
+                      int quantityOnStore, ProductCondition productCondition, double productWeight,
+                      boolean stillOnSale)
+    {
+        this.id = id;
+        this.name = name;
+        this.shortDesc = shortDesc;
+        this.longDesc = longDesc;
+        this.productPrice = productPrice;
+        this.quantityOnStore = quantityOnStore;
+        this.productCondition = productCondition;
+        this.productWeight = productWeight;
+        this.stillOnSale = stillOnSale;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    public String getLongDesc() {
+        return longDesc;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public int getQuantityOnStore() {
+        return quantityOnStore;
+    }
+
+    public ProductCondition getProductCondition() {
+        return productCondition;
+    }
+
+    public double getProductWeight() {
+        return productWeight;
+    }
+
+    public boolean isStillOnSale() {
+        return stillOnSale;
+    }
+}
