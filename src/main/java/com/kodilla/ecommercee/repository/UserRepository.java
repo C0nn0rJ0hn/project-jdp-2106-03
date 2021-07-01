75a80fafadc56bbd79f933ecc2813cdb75fe68ff
Paweł Piętoń <pawel.pieton@gmail.com>
2021-07-01 20:00:00 +0200

    Added User and UserRepository. Added simple Order class

---
+++ src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Override
+    User save(User user);
+
+    @Override
+    List<User> findAll();
+
+    @Override
+    Optional<User> findById(Long userId);
+
+    void deleteById(Long userId);
+}
+
