029d1a8cb22cf663b04cb9a01ed9fbbd9b466227
adam-p <77075408+adam-pietka@users.noreply.github.com>
2021-07-03 12:05:39 +0200

    in OrderRepository just added notation Transactional nad Repository

--- src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
@@ -3,9 +3,13 @@ package com.kodilla.ecommercee.repository;
 import com.kodilla.ecommercee.domain.Cart;
 import com.kodilla.ecommercee.domain.Order;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
+@Transactional
+@Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
     @Override
     Order save(Order order);
