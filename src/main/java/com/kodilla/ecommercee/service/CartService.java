283340051cbae9f51069ac305dadd4cbb5d1942b
adam-p <77075408+adam-pietka@users.noreply.github.com>
2021-07-18 00:38:38 +0200

    porapwki do CartService dla metody tworzącej zamówienie, oraz dla USER dla NIP -> zmiana na male litery bo NIP mnie zapisywał sie w tabeli

--- src/main/java/com/kodilla/ecommercee/service/CartService.java
+++ src/main/java/com/kodilla/ecommercee/service/CartService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -109,21 +110,21 @@ public class CartService {
         if (!activeUser.isBlocked()) {
 
             createdOrder.setCart(activeCart);
-            createdOrder.setOrderIsCompleted(true);
             createdOrder.setUser(activeUser);
+            createdOrder.setOrderTotalPrice(activeCart.getCartSum());
+            createdOrder.setOrderIsCompleted(false);
+            createdOrder.setOrderIsPaid(false);
+            createdOrder.setOrderDate(LocalDate.now());
+            createdOrder.setOrderNumber("INV/" + LocalDate.now() + "/" +  activeCart.getId());
 
             activeCart.setCartClosed(true);
-
             Cart newCart = new Cart();
-            activeUser.setCart(newCart);
+            activeUser.setCart(cartRepository.save(newCart));
 
-            cartRepository.save(newCart);
             cartRepository.save(activeCart);
             userRepository.save(activeUser);
-
-        } else { throw new UserIsBlockedException();}
-
-        return orderRepository.save(createdOrder);
+            return orderRepository.save(createdOrder);
+        }
+        throw new UserIsBlockedException();
     }
-
 }
